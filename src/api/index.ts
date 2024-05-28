bc28140125f1b8cf0e49bf8f8c3f5aa96ca4571e
Somayah-Absi <somayahahmedabsi@gmail.com>
2024-05-28 12:53:11 +0300

    try

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ let baseURL = "https://sda-online-2-csharp-backend-teamwork-8h7n.onrender.com/ap
 
 if (!isDevelopment) {
   // Update this later when you have a working backend server
-  baseURL = "https://sda-online-2-csharp-backend-teamwork-8h7n.onrender.com/api/"
+  baseURL = "https://sda-online-2-csharp-backend-teamwork-8h7n.onrender.com/api"
 }
 
 const api = axios.create({
